f585e58a0e191a5ec43b4a9b7a6dface92414c38
Lab10usuario28 <lab10usuario28@lab10maq28.academicos.com>
2019-07-25 15:47:41 -0300

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JokesService } from '../services/jokes.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private jokesService: JokesService) {}
+
+  piada: any;
+  url= 'http://www.mundogump.com.br/wp-content/uploads/2009/08/risada.jpg';
+  partePiada: string;
+  piadas: any[];
+  piadasPesquisadas: any[];
+
+  ngOnInit() {
+    // this.showJoke();
+    this.jokesService.getAllJokes().subscribe(piadas => {
+      this.piadas = piadas;
+    });
+  }
+
+  showJoke(){
+    this.jokesService.getAllJokes().subscribe(piadas => {
+      this.piada = piadas[this.indexAleatorio(piadas)];
+      this.piadas = piadas;
+    },erro => {
+      console.log(erro);
+    },() => {
+      console.log('sucesso!');
+    });
+  }
+
+  indexAleatorio(piadas) {
+    const index = Math.floor(Math.random() * piadas.length + 1);
+    return index;
+  }
+
+  pesquisarPiada(){
+    
+    this.piadasPesquisadas = this.piadas.filter((piada) => {
+      if(piada.pergunta.toLowerCase().indexOf(this.partePiada.toLowerCase()) > -1){
+        return piada;
+      }
+      else if(piada.resposta.toLowerCase().indexOf(this.partePiada.toLowerCase()) > -1){
+        return piada;
+      }
+    })
+  }
 
 }
